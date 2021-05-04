45eae658cb3492f7eaa0457b6459d4d8c8a9713a
grishma-ramoliya <grishmaramoliya1020@gmail.com>
2021-05-04 15:36:13 +0530

    Initial commit

--- app/src/main/java/com/example/musicvideoapp/activity/VideoActivity.java
+++ app/src/main/java/com/example/musicvideoapp/activity/VideoActivity.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.arthenica.mobileffmpeg.Config;
+import com.arthenica.mobileffmpeg.ExecuteCallback;
+import com.arthenica.mobileffmpeg.FFmpeg;
 import com.example.musicvideoapp.R;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -60,5 +71,4 @@ public class VideoActivity extends AppCompatActivity {
         btnStart=findViewById(R.id.btnStart);
 
     }
-
 }
