ed6c332e1b2e99114689a5579f45f349c66b925a
grishma-ramoliya <grishmaramoliya1020@gmail.com>
2021-05-19 11:58:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/musicvideoapp/activity/MyVideoActivity.java
@@ -0,0 +1,16 @@
+package com.example.musicvideoapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.musicvideoapp.R;
+
+public class MyVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_video);
+    }
+}
